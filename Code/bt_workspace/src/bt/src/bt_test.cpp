74908980aea29975d172a0f553e965a4f3d8e11e
mounteverset <lukas.e@web.de>
2022-07-25 09:56:53 +0200

    expand bt.xml

--- Code/bt_workspace/src/bt/src/bt_test.cpp
+++ Code/bt_workspace/src/bt/src/bt_test.cpp
@@ -1,6 +1,44 @@
 #include "behaviortree_cpp_v3/bt_factory.h"
 
+class RobotWatchdog : public BT::SyncActionNode
+{
+    public:
+        RobotWatchdog(const std::string& name) : BT::SyncActionNode(name, {})
+        {
+
+        }
+
+        BT::NodeStatus tick() override
+        {
+            std::cout << "Robot Watchdog: " << this->name() << std::endl;
+            return BT::NodeStatus::SUCCESS;
+        }
+};
+
+class LidarExecutionCheck: public BT::ConditionNode
+{
+    public: 
+    LidarExecutionCheck(const std::string& name): BT::ConditionNode(name, {})
+    {
+
+    }
+
+    BT::NodeStatus tick() override
+    {
+        std::cout << "Lidar is alive says: " << this->name() << std::endl;
+        return BT::NodeStatus::SUCCESS;
+    }
+};
+
 int main()
 {
+    BT::BehaviorTreeFactory factory;
+
+    factory.registerNodeType<LidarExecutionCheck>("LidarExecutionCheck");
+
+    auto tree = factory.createTreeFromFile("../../resources/bt.xml");
+
+    tree.tickRoot();
+
     return 0;
 }
